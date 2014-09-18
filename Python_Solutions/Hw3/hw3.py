111a05ff1e4b17eaa0cda0955645be2b95dd2684
stevenwalton <walton.stevenj@gmail.com>
2014-09-18 00:18:47 -0700

    Hw3 Solution, comments added

--- Python_Solutions/Hw3/hw3.py
+++ Python_Solutions/Hw3/hw3.py
@@ -1,17 +1,25 @@
+# We will only need these two modules for the entire homework (as usual)
 import numpy as np
 import matplotlib.pyplot as plt
 
 ### Problem 1 ###
 
-N = 1200    # resolution we will be using
+
+N = 1200                    # resolution we will be using, note that
+                            # this is so code is easily modifiable (style)
 x = np.linspace(-10,10,N)
 y = np.linspace(-10,10,N)
 
-X, Y = np.meshgrid(x,y)
+X, Y = np.meshgrid(x,y)     # We can define meshgrid this way
 
+# Note the use of np.sin and np.arctan and not math.sin/math.atan
+# The reason we do this is because numpy is able to handle arrays 
+# better than the normal math package.  It will not work with the math
+# package.  Trust me, I tried
 F = np.sin(X*Y)
 G = np.arctan(X/Y)
 
+# We want to do a contour plot of the function
 plt.contour(X,Y, (F-G),[0])
 plt.xlabel('x')
 plt.ylabel('y')
@@ -20,11 +28,11 @@ plt.show()
 
 ### Probelm 2 part b ###
 
-# Params given
+# Params given by problem
 a = 5*(10**(-3))
 y0 = 1
 
-# Step and scale
+# Step and scale (again, for modifiability)
 h = 0.1
 t = np.arange(0,600,h)
 
@@ -45,27 +53,31 @@ plt.plot(t,yExact,'b+')
 plt.xlabel('Time (s)')
 plt.ylabel('y (m)')
 # Calling plt.title as follows will show us current value of a
+# We are coders, and we write with style/laziness 
 plt.title('dy/dt=-ay, a = %f'%a)
 plt.show()
 
 
 ### Problem 3 ###
 
-# Given Parameters
+# Given Parameters from problem
 a = 5*(10**(-3))
 b = 1*(10**(-5))
 omega = 2 * np.pi/30
 y0 = 1
 
-# redefine t for new step size
+# redefine t for new step size (though we could have kept t the same
+# this is more about code style
 h = 0.01
 t = np.arange(0,600,h)
 
-# We do need to reset y though
+# We do need to reset y though (similar)
 y = np.zeros(len(t))
 y[0] = y0
 
 # Euler's
+# we could use the same indexing parameter, i, as above, but this is better
+# practice and style.  We don't want to confuse ourselves/others
 for n in range(1,len(t)):
    y[n] = y[n-1] - h*a*y[n-1] + b*np.cos(omega*t[n-1])
 
