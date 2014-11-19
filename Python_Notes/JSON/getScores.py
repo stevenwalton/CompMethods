e6b80678a038d5ec4fa8884513b4a6e868fb4fc1
stevenwalton <walton.stevenj@gmail.com>
2014-11-19 13:25:21 -0700

    Can not get team names from ESPNFC

--- Python_Notes/JSON/getScores.py
+++ Python_Notes/JSON/getScores.py
@@ -1,14 +1,23 @@
 import urllib2
 from bs4 import BeautifulSoup as BS
+from pprint import pprint
+import json
 
 url = "http://www.espnfc.com/scores?date=20141118"
 espnResponse = urllib2.urlopen(url)
 soup = BS(espnResponse.read())
 
 htmlteams = soup.find_all("div", {"class":"team-names"})
+teams = []
+for x in range(0,len(htmlteams)):
+    if htmlteams[x].span.string == None: 
+        teams.append(htmlteams[x].span.find('img')['alt'])
 
-
-
+    else:
+        teams.append(htmlteams[x].span.string)
+for x in range(0,len(teams)):
+    teams[x] = str(json.dumps(teams[x])).replace('"',"")
+    pprint(teams[x])
 
 #   What I know helps
 #   scoreteam = soup.find_all('div',{'class':'team-name'})
